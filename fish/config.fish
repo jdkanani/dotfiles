a6caab9f38dc620549c5193b51b7c42c2cae8455
Jaynti Kanani <jdkanani@gmail.com>
2018-05-13 15:03:26 +0530

    more config

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ end
 
 # aliases
 alias mux "tmuxinator"
+alias solc solcjs
 
 # editor
 set -x EDITOR nvim
@@ -11,10 +12,16 @@ set -x EDITOR nvim
 # GPG settings
 set -x GPG_TTY (tty)
 
-# Node path
-set PATH /usr/local/bin $HOME/.bin $PATH
+
+set -g -x LIBRARY_PATH $LIBRARY_PATH /usr/local/lib
+
+# Node and android path
+set PATH /usr/local/bin $HOME/.bin '$HOME/.android-sdk' $PATH
 
 # Setting ag as the default source for fzf
 set -x FZF_DEFAULT_COMMAND 'ag -g ""'
 set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+set -x ANDROID_SDK_HOME $HOME'/.android-sdk'
+set -x ANDROID_HOME $HOME'/.android-sdk'
 rvm default
+
